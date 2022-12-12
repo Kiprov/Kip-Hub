fdfb8337932edf3a81d99d06bf9399142f4ee4fc
Kiprov <118040079+Kiprov@users.noreply.github.com>
2022-12-12 07:31:08 +0300

    Delete Rush.lua

--- Rush.lua
+++
@@ -1,8 +0,0 @@
-local EntitySpawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/dreadmania/Scripts/main/Spawner_V2.lua"))()
-local Configuration = {
-    Kill = false, -- change to "Damage = 10," for eyes, doesnt work on other entities
-    Speed = 60, -- 60 for rush, doesnt work on other entities
-    Time = 5 -- 5 for rush, doesnt work on other entities
-}
-
-EntitySpawner:Spawn("Rush", Configuration)
