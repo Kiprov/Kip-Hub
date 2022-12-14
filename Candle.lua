bc43853bd673221800fb0d314dcca916caf17788
Kiprov <118040079+Kiprov@users.noreply.github.com>
2022-12-14 13:43:12 +0300

    Create Candle.lua

---
+++ Candle.lua
@@ -0,0 +1,63 @@
+local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
+		local CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))()
+		local Candle = game:GetObjects("rbxassetid://11630702537")[1]
+		Candle.Parent = game.Players.LocalPlayer.Backpack
+		local plr = game.Players.LocalPlayer
+		local Char = plr.Character or plr.CharacterAdded:Wait()
+		local Hum = Char:FindFirstChild("Humanoid")
+		local RightArm = Char:FindFirstChild("RightUpperArm")
+		local LeftArm = Char:FindFirstChild("LeftUpperArm")
+		local RightC1 = RightArm.RightShoulder.C1
+		local LeftC1 = LeftArm.LeftShoulder.C1
+		local AnimIdle = Instance.new("Animation")
+		AnimIdle.AnimationId = "rbxassetid://9982615727"
+		AnimIdle.Name = "IDleloplolo"
+		local cam = workspace.CurrentCamera
+		Candle.Handle.Top.Flame.GuidingLighteffect.EffectLight.LockedToPart = true
+		Candle.Handle.Material = Enum.Material.Salt
+		local track = Hum.Animator:LoadAnimation(AnimIdle)
+		track.Looped = true
+		local Equipped = false
+		for i, v in pairs(Candle:GetDescendants()) do
+			if v:IsA("BasePart") then
+				v.CanCollide = false
+			end
+		end
+		Candle.Equipped:Connect(function()
+			for _, v in next, Hum:GetPlayingAnimationTracks() do
+				v:Stop()
+			end
+			Equipped = true
+        -- RightArm.Name = "R_Arm"
+			track:Play()
+        -- RightArm.RightShoulder.C1 = RightC1 * CFrame.Angles(math.rad(-90), math.rad(-15), 0)
+		end)
+		Candle.Unequipped:Connect(function()
+			RightArm.Name = "RightUpperArm"
+			track:Stop()
+			Equipped = false
+        -- RightArm.RightShoulder.C1 = RightC1
+		end)
+		cam.ChildAdded:Connect(function(screech)
+			if screech.Name == "Screech" and math.random(1, 400) ~= 1 then
+				if not Equipped then
+					return
+				end
+				if Equipped then
+					game:GetService("Debris"):AddItem(screech, 0.05)
+				end
+			end
+		end)
+		Candle.TextureId = "rbxassetid://11622366799"
+    -- Create custom shop item
+		if plr.PlayerGui.MainUI.ItemShop.Visible then
+			CustomShop.CreateItem(Candle, {
+				Title = "Guiding Candle",
+				Desc = "The one and only light source...",
+				Image = "rbxassetid://11622366799",
+				Price = 75,
+				Stack = 1,
+			})
+		else
+			Candle.Parent = game.Players.LocalPlayer.Backpack
+		end
