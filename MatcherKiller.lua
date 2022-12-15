a49e24955e64701068472160c20b19ffe4e631be
Kiprov <118040079+Kiprov@users.noreply.github.com>
2022-12-15 07:08:20 +0300

    Delete MatcherKiller.lua

--- MatcherKiller.lua
+++
@@ -1,8 +0,0 @@
-local EntitySpawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiprov/Kip-Hub/main/EntitySpawner.lua"))()
-local Configuration = {
-    Kill = true,
-    Speed = 100,
-    Time = 5,
-}
-
-EntitySpawner:Spawn("Matcher", Configuration)
