cdd62f8ea0215b878e3350c2725f2044054fcd7b
Kiprov <118040079+Kiprov@users.noreply.github.com>
2023-12-21 12:09:28 +0300

    Update RedSkullAntiCheatTest.lua

--- RedSkullAntiCheatTest.lua
+++ RedSkullAntiCheatTest.lua
@@ -1,5 +1,7 @@
 print("hey ik ur there redskull")
 local brightLoop = nil
+local espParts = {}
+local partEspTrigger = nil
 if brightLoop then
 		brightLoop:Disconnect()
 	end
@@ -12,3 +14,24 @@ if brightLoop then
 	end
 
 	brightLoop = RunService.RenderStepped:Connect(brightFunc)
+
+local partEspName = "monster"
+	if not table.find(espParts,partEspName) then
+		table.insert(espParts,partEspName)
+		for i,v in pairs(workspace:GetDescendants()) do
+			if v:IsA("BasePart") and v.Name:lower() == partEspName then
+				local a = Instance.new("BoxHandleAdornment")
+				a.Name = partEspName.."_PESP"
+				a.Parent = v
+				a.Adornee = v
+				a.AlwaysOnTop = true
+				a.ZIndex = 0
+				a.Size = v.Size
+				a.Transparency = espTransparency
+				a.Color = BrickColor.new("Lime green")
+			end
+		end
+	end
+	if partEspTrigger == nil then
+		partEspTrigger = workspace.DescendantAdded:Connect(partAdded)
+end
