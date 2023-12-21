5f9afa2e80081312bea781216a6c40b68809ba65
Kiprov <118040079+Kiprov@users.noreply.github.com>
2023-12-21 13:13:52 +0300

    Update RedSkullAntiCheatTest.lua

--- RedSkullAntiCheatTest.lua
+++ RedSkullAntiCheatTest.lua
@@ -1,26 +1,6 @@
 print("hey ik ur there redskull")
 local brightLoop = nil
-local espParts = {}
-local partEspTrigger = nil
 local Lighting = game.Lighting
-function partAdded(part)
-	if #espParts > 0 then
-		if table.find(espParts,part.Name:lower()) then
-			local a = Instance.new("BoxHandleAdornment")
-			a.Name = part.Name:lower().."_PESP"
-			a.Parent = part
-			a.Adornee = part
-			a.AlwaysOnTop = true
-			a.ZIndex = 0
-			a.Size = part.Size
-			a.Transparency = 0
-			a.Color = BrickColor.new("Lime green")
-		end
-	else
-		partEspTrigger:Disconnect()
-		partEspTrigger = nil
-	end
-end
 if brightLoop ~= nil then
 		brightLoop:Disconnect()
 	end
@@ -34,23 +14,21 @@ if brightLoop ~= nil then
 
 	brightLoop = RunService.RenderStepped:Connect(brightFunc)
 
-local partEspName = "monster"
-	if not table.find(espParts,partEspName) then
-		table.insert(espParts,partEspName)
-		for i,v in pairs(workspace:GetDescendants()) do
-			if v:IsA("BasePart") and v.Name:lower() == partEspName then
-				local a = Instance.new("BoxHandleAdornment")
-				a.Name = partEspName.."_PESP"
-				a.Parent = v
-				a.Adornee = v
-				a.AlwaysOnTop = true
-				a.ZIndex = 0
-				a.Size = v.Size
-				a.Transparency = 0
-				a.Color = BrickColor.new("Lime green")
-			end
-		end
-	end
-	if partEspTrigger == nil then
-		partEspTrigger = workspace.DescendantAdded:Connect(partAdded)
+workspace.DescendantAdded:Connect(function(part)
+if part:IsA("BasePart") and part.Name == "monster" then
+game.StarterGui:SetCore("SendNotification", {
+            Title = "Monster Spawned ⚠️",
+            Text = "Hide quick",
+            Duration = 5
+        })
 end
+end)
+workspace.DescendantRemoved:Connect(function(part)
+if part:IsA("BasePart") and part.Name == "monster" then
+game.StarterGui:SetCore("SendNotification", {
+            Title = "Monster Despawned ✅",
+            Text = "ur good",
+            Duration = 5
+        })
+end
+end)
