b9ba8b5a8ad8289ff3fe714ac3d3f8bf74792406
Kiprov <118040079+Kiprov@users.noreply.github.com>
2023-12-21 11:57:10 +0300

    Update RedSkullAntiCheatTest.lua

--- RedSkullAntiCheatTest.lua
+++ RedSkullAntiCheatTest.lua
@@ -1,2 +1,14 @@
 print("hey ik ur there redskull")
-game.Lighting.Brightness = 10
+local brightLoop = nil
+if brightLoop then
+		brightLoop:Disconnect()
+	end
+	local function brightFunc()
+		Lighting.Brightness = 2
+		Lighting.ClockTime = 14
+		Lighting.FogEnd = 100000
+		Lighting.GlobalShadows = false
+		Lighting.OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+	end
+
+	brightLoop = RunService.RenderStepped:Connect(brightFunc)
